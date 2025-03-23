e0241b182d36acb5ec7b50710d5502fc31c3e975
igalaxy <hey@igalaxy.dev>
2025-03-23 15:42:44 -0700

    init template

---
+++ src/client/java/mom/wii/configurablestatisticunits/ConfigurableStatisticUnitsClient.java
@@ -0,0 +1,10 @@
+package mom.wii.configurablestatisticunits;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ConfigurableStatisticUnitsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
